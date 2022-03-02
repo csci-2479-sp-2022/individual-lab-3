93c4a44c49c3f6102ad5c80f69807f1fedf9c8a5
Joe_Robbins <sbcjoe@gmail.com>
2022-03-02 09:03:08 -0500

    other tests

--- tests/Feature/PetControllerTest.php
+++ tests/Feature/PetControllerTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature;
 
 use App\Contracts\PetService;
 use App\Models\Pet;
-use App\Http\Controllers\PetController;
+
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Mockery\MockInterface;
@@ -16,37 +16,6 @@ class PetControllerTest extends TestCase
     private MockInterface $petServiceSpy;
 
     private $pets = [];
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->pets = self::getPets();
-
-        $this->petServiceSpy = $this->spy(PetService::class);
-        
-    }
-
-    public function test_petsRoute_returns_successful()
-    {
-        // arrange
-
-        $this->petServiceSpy->shouldRecieve('getPets')
-            ->once()
-            ->andReturn($pets);
-
-        // act
-        $response = $this->get('/pets');
-
-        // assert
-        $response->assertStatus(200);
-        $response->assertViewHas('pets', [
-            new Pet (1, 'Fido', 5, Pet::PET_TYPE_DOG)
-        ]);
-                
-    }
-
-
 
     private static function getPets(): array
     {
@@ -65,4 +34,65 @@ class PetControllerTest extends TestCase
             ]),
         ];
     }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->pets = self::getPets();
+
+        $this->petServiceSpy = $this->spy(PetService::class);
+        
+    }
+
+    public function test_petsRoute_returns_successful()
+    {
+        // arrange
+        $this->petServiceSpy->shouldReceive('getPets')
+            ->once()
+            ->andReturn($this->pets);
+
+        // act
+        $response = $this->get('/pets');
+
+        // assert
+        $response->assertStatus(200);
+        $response->assertViewHas('pets', $this->pets);
+                
+    }
+
+    public function test_petRoute_returns_successful()
+    {
+        // arrange
+        $this->petServiceSpy->shouldReceive('getPetById')
+            ->once()
+            ->andReturn($this->pets[0]);
+
+        // act
+        $response = $this->get('/pets/1');
+
+        // assert
+        $response->assertStatus(200);
+        $response->assertViewHas('pet', $this->pets[0]);
+                
+    }
+
+    public function test_petRoute_returns_unsuccessful()
+    {
+        // arrange
+        $this->petServiceSpy->shouldReceive('getPetById')
+            ->once()
+            ->andReturn($this->pets[0]);
+
+        // act
+        $response = $this->get('/pets/5');
+
+        // assert
+        $response->assertStatus(404);
+                
+    }
+
+
+
+
 }
