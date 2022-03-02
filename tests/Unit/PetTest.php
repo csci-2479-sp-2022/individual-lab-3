7f98c5ab1df332bc82e667ddee81ea0fc95f8bf4
Mihaela Valac <mvalac@student.cscc.edu>
2022-03-01 22:15:57 -0500

    Created Unit and Feature tests.

---
+++ tests/Unit/PetTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Pet;
+use Tests\TestCase;
+
+
+class PetTest extends TestCase {
+
+
+
+    public function test_expectedString()
+    {
+        //arrange
+        $pet = Pet::make([
+            'id' => 1,
+            'name' => 'Fido',
+            'age' => 5,
+            'type' => Pet::PET_TYPE_DOG,
+        ]);
+        $expectedString = "name: Fido, age: 5, type: dog";
+
+        // act
+        $actualString = $pet->toString();
+
+        // assert
+        $this->assertEquals($expectedString, $actualString);
+    }
+}
